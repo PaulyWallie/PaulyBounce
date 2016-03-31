c95c41e73edf3d10b84a2e70531072f30e882387
Paul Kantoski <unkleyoyo@yahoo.com>
2016-03-30 22:52:11 -0400

    Starting Template
    I Follow a tutorial from Awesome Tuts on Udemy to make a game called
    Cowboy Runner. I started on 3/21/2016  and finished it on 3/30/2016. I
    plan to use this as a starting point to create a game called
    PaulyBounce, an endless runner type of game. Hopefully this starting
    point will be useful!

---
+++ PaulyBounce/Assets/Scripts/Background Scripts/BGLooper.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGLooper : MonoBehaviour {
+
+	public float speed = 0.1f;
+
+	private Vector2 offSet = Vector2.zero;
+	private Material mat;
+
+	void Start () {
+		mat = GetComponent<Renderer> ().material;
+		offSet = mat.GetTextureOffset("_MainTex");
+	}
+	
+
+	void Update () {
+		offSet.x += speed * Time.deltaTime;
+		mat.SetTextureOffset ("_MainTex", offSet);
+	}
+}
---
+++ PaulyBounce/Assets/Scripts/Game Controllers/GameplayController.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameplayController : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject pausePanel;
+
+	[SerializeField]
+	private Button restartGameButton;
+
+	[SerializeField]
+	private Text scoreText, pauseText;
+
+	private int score;
+
+	void Start () {
+		scoreText.text = score + "M";
+		StartCoroutine (CountScore());
+	}
+
+	IEnumerator CountScore() {
+		yield return new WaitForSeconds(0.6f);
+		score++;
+		scoreText.text = score + "M";
+		StartCoroutine (CountScore());
+	}
+
+	void OnEnable(){
+		PlayerDied.endGame += PlayerDiedEndTheGame;
+	}
+
+	void OnDisable(){
+		PlayerDied.endGame -= PlayerDiedEndTheGame;
+	}
+
+
+	void PlayerDiedEndTheGame() {
+		if (!PlayerPrefs.HasKey ("Score")){
+			PlayerPrefs.SetInt("Score", 0);
+		} else {
+			int highscore = PlayerPrefs.GetInt("score");
+
+			if(highscore < score) {
+				PlayerPrefs.SetInt("Score", score);
+			}
+		}
+
+		pauseText.text = "Game Over";
+		pausePanel.SetActive (true);
+		restartGameButton.onClick.RemoveAllListeners();
+		restartGameButton.onClick.AddListener(() => RestartGame());
+		Time.timeScale = 0f;
+	}
+
+	public void PauseButton() {
+		Time.timeScale = 0f;
+		pausePanel.SetActive (true);
+		restartGameButton.onClick.RemoveAllListeners();
+		restartGameButton.onClick.AddListener(() => RestartGame());
+	}
+
+	public void GoToMenu() {
+		Time.timeScale = 1f;
+		Application.LoadLevel("MainMneu");
+	}
+
+	public void ResumeGame() {
+		Time.timeScale = 1f;
+		pausePanel.SetActive (false);
+	}
+
+	public void RestartGame() {
+		Time.timeScale = 1f;
+		Application.LoadLevel ("Gameplay");
+	}
+
+} // GameplayController
