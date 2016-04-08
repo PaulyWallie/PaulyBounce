e74d9632ababc09e7e0710d848ecb8fc4d22da1d
Paul Kantoski <unkleyoyo@yahoo.com>
2016-04-07 20:58:53 -0400

    edited code
    I edited a script so the hands objects that are going to be platforms(
    sprites not in the project yet) be randomly generated between two point
    on the y axis.

--- PaulyBounce/Assets/Scripts/Obstacles Script/ObstacleSpawner.cs
+++ PaulyBounce/Assets/Scripts/Obstacles Script/ObstacleSpawner.cs
@@ -9,6 +9,8 @@ public class ObstacleSpawner : MonoBehaviour {
 
 	private List<GameObject> obstaclesForSpawning = new List<GameObject>();
 
+	public float minWait, maxWait, minY, maxY;
+
 	void Awake () {
 		InitializeObstalces();
 	}
@@ -21,7 +23,7 @@ public class ObstacleSpawner : MonoBehaviour {
 	int index = 0;
 	for (int i = 0; i < obstacles.Length * 3; i ++) {
 		GameObject obj = Instantiate(obstacles[index], new Vector3(transform.position.x,
-																   transform.position.y, -2), Quaternion.identity) as GameObject;
+																   transform.position.y, 0), Quaternion.identity) as GameObject;
 		obstaclesForSpawning.Add(obj);
 		obstaclesForSpawning[i].SetActive (false);
 		index ++;
@@ -40,13 +42,13 @@ public class ObstacleSpawner : MonoBehaviour {
 	}
 
 	IEnumerator SpawnRandomObstacles() {
-		yield return new WaitForSeconds (Random.Range(1.5f, 4.5f));
+		yield return new WaitForSeconds (Random.Range(minWait, maxWait));
 	
 		int index = Random.Range(0, obstaclesForSpawning.Count);
 		while (true) {
 			if(!obstaclesForSpawning[index].activeInHierarchy) {
 				obstaclesForSpawning[index].SetActive(true);
-				obstaclesForSpawning[index].transform.position = new Vector3(transform.position.x, transform.position.y, -2);
+				obstaclesForSpawning[index].transform.position = new Vector3(transform.position.x, Random.Range(minY,maxY), 0);
 				break;
 			} else {
 				index = Random.Range(0, obstaclesForSpawning.Count);
